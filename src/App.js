4ee2cf807322b6e30a166ae77d737da22902a419
Guro Brøto <gurobroto@dhcp-10-22-31-124.wlan.ntnu.no>
2019-02-13 17:17:48 +0100

    Reorganize files, adding test-layer

--- src/App.js
+++ src/App.js
@@ -1,51 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import mapboxgl from 'mapbox-gl'
-
-mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
-
-class Map extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      lng: 5,
-      lat: 34,
-      zoom: 1.5
-    };
-  }
-
-  componentDidMount() {
-    const { lng, lat, zoom } = this.state;
-
-    const map = new mapboxgl.Map({
-      container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v9',
-      center: [lng, lat],
-      zoom
-    });
-
-     map.on('move', () => {
-      const { lng, lat } = map.getCenter();
-
-      this.setState({
-        lng: lng.toFixed(4),
-        lat: lat.toFixed(4),
-        zoom: map.getZoom().toFixed(2)
-      });
-    });
-  }
-
-  render() {
-    const { lng, lat, zoom } = this.state;
-
-    return (
-        <div className="map" ref={el => this.mapContainer = el} />
-    );
-  }
-}
-
-
+import React, { Component } from 'react'
+import './App.css'
+import Map from './Map/Map.js'
 
 class App extends Component {
   render() {
